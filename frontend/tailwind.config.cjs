03b5e00593c48e157289ea3bc13f0462fc0391e5
mrantonio031 <yu-xuan.kang@outlook.com>
2025-11-12 21:56:53 +0100

    Prototype with mock data

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -1,13 +1,11 @@
 /** @type {import('tailwindcss').Config} */
-// ...existing code...
 module.exports = {
   content: [
     "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}"
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
-// ...existing code...
+}
